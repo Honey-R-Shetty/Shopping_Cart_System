a3dc57e780219c9adfd62887e79b7b54a27d1493
Honey R Shetty <honeyshetty2003@gmail.com>
2025-09-01 13:25:58 +0530

    add_item

---
+++ src/main/java/com/cart/Item.java
@@ -0,0 +1,32 @@
+package com.cart;
+
+public class Item {
+	private int id;
+	private String name;
+	private double price;
+
+	public Item(int id, String name, double price) {
+		if (id <= 0 || name.trim() == null || price <= 0) {
+			throw new IllegalArgumentException("Invalid input!!");
+		}
+		this.id = id;
+		this.name = name;
+		this.price = price;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public double getPrice() {
+		return price;
+	}
+
+	public String displayItem() {
+		return "Item [id=" + id + ", name=" + name + ", price=" + price + "]";
+	}
+}
